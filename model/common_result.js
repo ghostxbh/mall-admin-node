2311c4b02b5aa9aa7899cf45abdfbc248afcc40c
ghostxbh <ghostxbh@hotmail.com>
2019-05-28 09:58:27 +0800

    目录提交

---
+++ model/common_result.js
@@ -0,0 +1,14 @@
+/**
+ * Create by xbh 2019-05-28 通用返回对象
+ */
+//操作成功
+const SUCCESS = 200;
+//操作失败
+const FAILED = 500;
+//参数校验失败
+const VALIDATE_FAILED = 404;
+//未认证
+const UNAUTHORIZED = 401;
+//未授权
+const FORBIDDEN = 403;
+
