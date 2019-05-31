2b2e910975278f607f0df3a9ed933d02276d38a8
ghostxbh <ghostxbh@hotmail.com>
2019-05-31 21:46:54 +0800

    角色dao层修改

--- dao/role-dao.js
+++ dao/role-dao.js
@@ -1,6 +1,7 @@
 /**
  * Create by xbh 2019-05-29 role CRUD
  */
+const fields = 'id,name,description,admin_count,create_time,status,sort';
 const Mysql = require('../util/mysql-util');
 
 const roleDao = {
@@ -19,7 +20,7 @@ const roleDao = {
         if (name) set += `name='${name}',`;
         if (description) set += `description='${description}',`;
         if (adminCount || adminCount === 0) set += `admin_count=${adminCount},`;
-        if (status) set += `status=${status},`;
+        if (status || status === 0) set += `status=${status},`;
         if (sort || sort === 0) set += `sort=${sort}`;
         if (set.substring(set.length - 1, set.length) === ',') set = set.substring(0, set.length - 1);
         let sql = `update ums_role set ${set} where id=${id}`;
@@ -32,7 +33,7 @@ const roleDao = {
     },
     //列表
     list() {
-        let sql = `select id,name,description,admin_count,create_time,status,sort from ums_role`;
+        let sql = `select ${fields} from ums_role`;
         return Mysql.fetch(sql);
     },
 };
