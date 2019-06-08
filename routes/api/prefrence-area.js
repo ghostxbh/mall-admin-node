0efaf0b53013b50e9a07e8dd0b0a8312b04fc25d
ghostxbh <ghostxbh@hotmail.com>
2019-06-08 22:49:10 +0800

    全部路由添加

---
+++ routes/api/prefrence-area.js
@@ -0,0 +1,8 @@
+/**
+ * Create by xbh 2019-06-06
+ */
+var express = require('express');
+var router = express.Router();
+const result = require('../../model/common-result');
+
+module.exports = router;
